f99d307cee0d9dcf547371aab0a1026ea17f8192
SaptakBhoumik <saptakbhoumik@gmail.com>
2021-12-28 15:00:50 +0530

    .

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -25,9 +25,9 @@ size_t strlen(const char* str)
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
 
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
+size_t terminal_row = 0;
+size_t terminal_column = 0;
+uint8_t terminal_color = 0;
 uint16_t* terminal_buffer;
 
 void terminal_initialize(void)

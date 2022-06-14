724527e4fef468eff69e3d0f0d4e40a867d26271
SaptakBhoumik <saptakbhoumik@gmail.com>
2022-06-15 00:54:51 +0530

    .

--- build.py
+++ build.py
@@ -1,8 +1,9 @@
 from os import system
-
-system("peregrine.elf compile kernel/kernel.pe")
+system("mkdir -p build")
+system("peregrine.elf compile kernel/kernel.pe -emit_cpp -o temp.cc")
 system("nasm -f elf asm/kernal/boot.asm -o build/boot.o")
 system("g++ -c temp.cc -o build/kernel.o -m32 -ffreestanding -fno-exceptions -fno-rtti")
 system("gcc build/boot.o build/kernel.o -T linker.ld -o iso/boot/toyos -nostdlib -nodefaultlibs -lgcc -m32")
 system("grub-mkrescue iso --output=toy.iso")
 system("qemu-system-x86_64 -boot d  -cdrom toy.iso -m 200")
+system("rm build/*")
